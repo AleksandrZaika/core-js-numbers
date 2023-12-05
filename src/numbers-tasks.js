88335a624632e40b7651269068b25f8a9607c738
AleksandrZ <saniokzaika@gmail.com>
2023-12-05 22:49:57 +0000

    17 tasks complete

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -185,8 +185,9 @@ function getParallelepipedDiagonal(a, b, c) {
  *   1678, 2  => 1700
  *   1678, 3  => 2000
  */
-function roundToPowerOfTen(/* num, pow */) {
-  throw new Error('Not implemented');
+function roundToPowerOfTen(num, pow) {
+  const multiplier = 10 ** pow;
+  return Math.round(num / multiplier) * multiplier;
 }
 
 /**
@@ -206,8 +207,16 @@ function roundToPowerOfTen(/* num, pow */) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  if (n < 2) {
+    return false;
+  }
+  for (let i = 2; i <= Math.sqrt(n); i += 1) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
 }
 
 /**
@@ -225,8 +234,13 @@ function isPrime(/* n */) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  const convertValue = Number(value);
+
+  if (!Number.isNaN(convertValue)) {
+    return convertValue;
+  }
+  return def;
 }
 
 /**
@@ -240,8 +254,8 @@ function toNumber(/* value, def */) {
  *   -2 => -8
  *   0  => 0
  */
-function getCube(/* num */) {
-  throw new Error('Not implemented');
+function getCube(num) {
+  return num ** 3;
 }
 
 /**
@@ -257,8 +271,14 @@ function getCube(/* num */) {
  *   3  => 2
  *   10 => 55
  */
-function getFibonacciNumber(/* index */) {
-  throw new Error('Not implemented');
+function getFibonacciNumber(index) {
+  const fibonSeq = [0, 1];
+
+  for (let i = 2; i <= index; i += 1) {
+    fibonSeq[i] = fibonSeq[i - 1] + fibonSeq[i - 2];
+  }
+
+  return fibonSeq[index];
 }
 
 /**
@@ -272,8 +292,14 @@ function getFibonacciNumber(/* index */) {
  *   10 => 55 // (1+2+3+...+10)
  *   1  => 1
  */
-function getSumToN(/* n */) {
-  throw new Error('Not implemented');
+function getSumToN(n) {
+  let sumNumbers = 0;
+
+  for (let i = 1; i <= n; i += 1) {
+    sumNumbers += i;
+  }
+
+  return sumNumbers;
 }
 
 /**
